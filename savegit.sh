ee1a0dfd0dc795de210ae7331838cc9aa817ca6a
Danilo Silva <danilocgsilva@gmail.com>
2018-05-13 13:57:00 -0300

    Now checks also if there was provided a valid working treee

--- savegit.sh
+++ savegit.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
+## The main entrance for the utility
 savegit () {
-	if [ -z "$1" ]; then
-		echo Please, provides the commit comment.
-		exit
-	fi
+	check_if_is_in_repository
+	check_if_argument_was_provided $1
 
 	git add -A
 	git commit -m "$1"
 	git push 
 }
 
+## Check if the folder is inside a git repository
+check_if_is_in_repository() {
+	if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+		echo The directory is not part of any work tree
+		exit
+	fi
+}
 
+## Check if the user provided the first argument
+check_if_argument_was_provided() {
+	if [ -z "$1" ]; then
+		echo Please, provides the commit comment.
+		exit
+	fi
+}
 
 ## detect if being sourced and
 ## export if so else execute
